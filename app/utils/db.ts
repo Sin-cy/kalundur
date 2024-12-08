e24b8e2d3fe88ce9dfd2e99f85c7d2af20c84949
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-12-08 15:55:20 +0700

    setup databases and authentication (Prisma with Supabase Postgres database)

---
+++ app/utils/db.ts
@@ -0,0 +1,16 @@
+// this makes sure we dont create a new connection to our database everytime the code reloads
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
