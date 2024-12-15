0eea4b1cd5832aab353fc32fdef8de833556722f
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-12-15 20:53:07 +0700

    ongoing setup of dashboard/new/page.tsx + created custom ButtonGroup.tsx component + added shadcn textarea component

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -1,34 +1,66 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 
-export default function NewEventRoute(){
+export default function NewEventRoute() {
   return (
-    <div className="w-full h-full flex flex-1 items-center justify-center">
+    <div className="flex h-full w-full flex-1 items-center justify-center">
       <Card>
         <CardHeader>
           <CardTitle>Add new appointment type</CardTitle>
-          <CardDescription>appointment that allows other people to book you</CardDescription>
+          <CardDescription>
+            appointment that allows other people to book you
+          </CardDescription>
         </CardHeader>
         <form>
           <CardContent className="grid gap-y-5">
             <div className="flex flex-col gap-y-2">
-              <Label >Title</Label>
-              <Input placeholder="30 mins meeting"/>
+              <Label>Title</Label>
+              <Input placeholder="30 mins meeting" />
             </div>
-            <div className="flex flex-col gap-y-2" >
+            <div className="flex flex-col gap-y-2">
               <Label>URL Slug</Label>
-              <div className="flex items-center rounded-md">
-                <span>
+              <div className="flex rounded-md">
+                <span className="inline-flex items-center rounded-l-md border border-r-0 border-muted bg-muted px-3 text-sm text-muted-foreground">
                   Kalundur.com/
                 </span>
-                <Input placeholder="example-url-1"/>
+                <Input className="rounded-l-none" placeholder="example-url-1" />
               </div>
+            </div>
+
+            <div className="flex flex-col gap-y-2 ">
+              <Label>Description</Label>
+              <Textarea placeholder="enter you message..."/>
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Duration</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select duration"/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Duration</SelectLabel>
+                    <SelectItem value="15" >15 mins</SelectItem>
+                    <SelectItem value="30" >30 mins</SelectItem>
+                    <SelectItem value="45" >45 mins</SelectItem>
+                    <SelectItem value="60" >1 hour</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
           </CardContent>
         </form>
       </Card>
-
     </div>
-  )
+  );
 }
---
+++ components/ui/ButtonGroup.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import { ButtonProps } from "./button";
+import { Children, cloneElement, ReactElement } from "react";
+
+interface buttonGroupProps {
+  className?: string;
+  children: ReactElement<ButtonProps>[];
+}
+
+// Creating Button Groups
+export function ButtonGroup({ className, children }: buttonGroupProps) {
+  // calculate the total button in the array
+  const totalButtons = Children.count(children);
+  return (
+    <div className={cn("flex w-full", className)}>
+      {children.map((child, index) => {
+        // boolean check first and lastItem for truthy values
+        // check when the button is first and last in the array
+        const isFirstItem = index === 0;
+        const isLastItem = index === totalButtons - 1;
+
+        // creates a new element
+        return cloneElement(child, {
+          className: cn({
+            "rounded-l-none": !isFirstItem, // firstItems is not true - no left rounding
+            "rounded-r-none": !isLastItem, // lastItems is not true - no right rouding
+            "border-l-zero": !isFirstItem, // firstItems is not true - no left border
+          }, child.props.className),
+        });
+      })}
+    </div>
+  );
+}
