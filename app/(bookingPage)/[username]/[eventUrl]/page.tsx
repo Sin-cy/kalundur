cee35a6e974f5165a3c485f96177168e55409db3
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-12-23 21:24:26 +0700

    finish TimeTable 5th Grid layout + created redirect to a new Form page from time selected from timetable

--- app/(bookingPage)/[username]/[eventUrl]/page.tsx
+++ app/(bookingPage)/[username]/[eventUrl]/page.tsx
@@ -6,6 +6,9 @@ import { CalendarX2, Clock2, VideoIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { TimeTable } from "@/app/components/bookingForm/TimeTable";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { SubmitButton } from "@/app/components/SubmitButtons";
 
 async function getData(eventUrl: string, userName: string) {
   // using findFirst cuz we dont have a unique identifier
@@ -51,9 +54,9 @@ export default async function BookingFormRoute({
   searchParams,
 }: {
   params: { username: string; eventUrl: string };
-  searchParams: { date?: string };
+  searchParams: { date?: string; time?: string };
   // we can get them through the params - our dynamic routes username and eventUrl (make sure its spelled exactly the same)
-    // whenever our routes are dynamic - we can get the data through the params
+  // whenever our routes are dynamic - we can get the data through the params
 }) {
   // for Next JS 15
   // Await params if it's asynchronous
@@ -65,91 +68,176 @@ export default async function BookingFormRoute({
   const data = await getData(eventUrl, username);
 
   // INFO: Making the date in the first grid section dynamic
-  // use to update the first grid section date with selected date from Calendar 
+  // use to update the first grid section date with selected date from Calendar
   const selectedDate = searchParams.date // we cant get searchParams as its a js client side bundle, we have to get it via Serverside
     ? new Date(searchParams.date)
     : new Date();
 
   // give the dynamic date some formatting
   const formattedDate = new Intl.DateTimeFormat("en-US", {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  }).format(selectedDate)
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  }).format(selectedDate);
+
+  // NOTE: checks if both data and time are true
+  // we dont have .time yet, so we need to get it through searchParams
+  // double !! converts any value to a BOOLEAN
+  const showForm = !!searchParams.date && !!searchParams.time
 
   return (
     <div className="flex min-h-screen w-screen items-center justify-center">
-      <Card className="mx-auto w-full max-w-[1000px]">
-        {/* this creating the Card Content as a grid is a little tricky */}
-        <CardContent className="gap-4 p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr]">
-          {/* First Grid Section */}
-          <div>
-            <Image
-              src={data.User?.image as string}
-              alt="Profile Image of User"
-              priority
-              width={60}
-              height={60}
-              className="rounded-full"
-            />
-            <p className="mt-2 text-sm font-medium text-muted-foreground">
-              {data.User?.name}
-            </p>
-            <h1 className="mt-2 text-xl font-semibold">
-              {data.title}
-            </h1>
-            <p className="text-sm font-medium text-muted-foreground">
-              {data.description}
-            </p>
 
-            <div className="Image m-5 flex flex-col gap-y-3">
-              <p className="flex items-center">
-                <CalendarX2 className="mr-2 size-4 text-primary" />
-                <span className="text-sm font-medium text-muted-foreground">
-                  {/* will hard code this for now - change later once the calendar is setup */}
-                  {formattedDate} 
-                </span>
+      {showForm ? (
+
+        <Card className="max-w-[600px] w-full ">
+          {/* this creating the Card Content as a grid is a little tricky */}
+          <CardContent className="gap-4 p-5 md:grid md:grid-cols-[1fr,auto,1fr]">
+            {/* First Grid Section */}
+            <div>
+              <Image
+                src={data.User?.image as string}
+                alt="Profile Image of User"
+                priority
+                width={60}
+                height={60}
+                className="rounded-full"
+              />
+              <p className="mt-2 text-sm font-medium text-muted-foreground">
+                {data.User?.name}
+              </p>
+              <h1 className="mt-2 text-xl font-semibold">
+                {data.title}
+              </h1>
+              <p className="text-sm font-medium text-muted-foreground">
+                {data.description}
               </p>
 
-              <p className="flex items-center">
-                <Clock2 className="mr-2 size-4 text-primary" />
-                <span className="text-sm font-medium text-muted-foreground">
-                  {data.duration} Minutes
-                </span>
-              </p>
+              <div className="Image m-5 flex flex-col gap-y-3">
+                <p className="flex items-center">
+                  <CalendarX2 className="mr-2 size-4 text-primary" />
+                  <span className="text-sm font-medium text-muted-foreground">
+                    {/* will hard code this for now - change later once the calendar is setup */}
+                    {formattedDate}
+                  </span>
+                </p>
 
-              <p className="flex items-center">
-                <VideoIcon className="mr-2 size-4 text-primary" />
-                <span className="text-sm font-medium text-muted-foreground">
-                  {data.videoCallSoftware}
-                </span>
-              </p>
+                <p className="flex items-center">
+                  <Clock2 className="mr-2 size-4 text-primary" />
+                  <span className="text-sm font-medium text-muted-foreground">
+                    {data.duration} Minutes
+                  </span>
+                </p>
+
+                <p className="flex items-center">
+                  <VideoIcon className="mr-2 size-4 text-primary" />
+                  <span className="text-sm font-medium text-muted-foreground">
+                    {data.videoCallSoftware}
+                  </span>
+                </p>
+              </div>
             </div>
-          </div>
 
-          {/* Second Grid Section (separator) */}
-          <Separator
-            orientation="vertical"
-            className="h-[full] w-[1px]"
-          />
+            {/* Second Grid Section (separator) */}
+            <Separator
+              orientation="vertical"
+              className="h-[full] w-[1px]"
+            />
 
-          {/* Third Grid Section (the custom calendar via React Aria) */}
-          <RenderCalendar
-            availability={data.User?.availability as any}
-          />
+            <form className="flex flex-col gap-y-4 ">
+              <div className="flex flex-col gap-y-2 mt-5 ">
+                <Label>Your Name</Label>
+                <Input placeholder="Enter Your Name" />
+              </div>
+              <div className="flex flex-col gap-y-2 ">
+                <Label>Your Email</Label>
+                <Input placeholder="JohnDoe@exmaple.com" />
+              </div>
+              <SubmitButton className="w-full mt-7" text="Book Meeting" />
 
-          {/* Fourth Grid Section Separator */}
-          <Separator
-            orientation="vertical"
-            className="h-[full] w-[1px]"
-          />
+            </form>
 
+          </CardContent>
+        </Card>
 
-          {/* Fifth Grid Section - Time Table */}
-          <TimeTable selectedDate={selectedDate} userName={params.username}/>
+      ) : (
 
-        </CardContent>
-      </Card>
+        <Card className="mx-auto w-full max-w-[1000px]">
+          {/* this creating the Card Content as a grid is a little tricky */}
+          <CardContent className="gap-4 p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr]">
+            {/* First Grid Section */}
+            <div>
+              <Image
+                src={data.User?.image as string}
+                alt="Profile Image of User"
+                priority
+                width={60}
+                height={60}
+                className="rounded-full"
+              />
+              <p className="mt-2 text-sm font-medium text-muted-foreground">
+                {data.User?.name}
+              </p>
+              <h1 className="mt-2 text-xl font-semibold">
+                {data.title}
+              </h1>
+              <p className="text-sm font-medium text-muted-foreground">
+                {data.description}
+              </p>
+
+              <div className="Image m-5 flex flex-col gap-y-3">
+                <p className="flex items-center">
+                  <CalendarX2 className="mr-2 size-4 text-primary" />
+                  <span className="text-sm font-medium text-muted-foreground">
+                    {/* will hard code this for now - change later once the calendar is setup */}
+                    {formattedDate}
+                  </span>
+                </p>
+
+                <p className="flex items-center">
+                  <Clock2 className="mr-2 size-4 text-primary" />
+                  <span className="text-sm font-medium text-muted-foreground">
+                    {data.duration} Minutes
+                  </span>
+                </p>
+
+                <p className="flex items-center">
+                  <VideoIcon className="mr-2 size-4 text-primary" />
+                  <span className="text-sm font-medium text-muted-foreground">
+                    {data.videoCallSoftware}
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            {/* Second Grid Section (separator) */}
+            <Separator
+              orientation="vertical"
+              className="h-[full] w-[1px]"
+            />
+
+            {/* Third Grid Section (the custom calendar via React Aria) */}
+            <RenderCalendar
+              availability={data.User?.availability as any}
+            />
+
+            {/* Fourth Grid Section Separator */}
+            <Separator
+              orientation="vertical"
+              className="h-[full] w-[1px]"
+            />
+
+            {/* Fifth Grid Section - Time Table */}
+            <TimeTable
+              selectedDate={selectedDate}
+              userName={params.username}
+              duration={data.duration}
+            />
+          </CardContent>
+        </Card>
+
+      )}
+
     </div>
   );
 }
--- app/components/bookingForm/TimeTable.tsx
+++ app/components/bookingForm/TimeTable.tsx
@@ -1,30 +1,33 @@
 import prisma from "@/app/utils/db";
 import { nylas } from "@/app/utils/nylas";
+import { Button } from "@/components/ui/button";
 import { Prisma } from "@prisma/client";
-import { format } from "date-fns";
+import { addMinutes, format, fromUnixTime, isAfter, isBefore, parse } from "date-fns";
+import Link from "next/link";
+import { GetFreeBusyRequest, GetFreeBusyResponse } from "nylas";
 
-async function getData(userName: string, selectedDate: Date){
+async function getData(userName: string, selectedDate: Date) {
   // format the selectedDate object for Dynamic dates
-  const currentDay = format(selectedDate , "EEEE")
+  const currentDay = format(selectedDate, "EEEE");
 
   // NOTE: used with nylas date data fetching
-  const startOfDay = new Date(selectedDate)
-  startOfDay.setHours(0,0,0,0) // this creates a new date object and starts at midnight
-  const endOfDay = new Date(selectedDate)
-  endOfDay.setHours(23,59,59,999) // this creates a new date object and ends 1ms just before midnight
+  const startOfDay = new Date(selectedDate);
+  startOfDay.setHours(0, 0, 0, 0); // this creates a new date object and starts at midnight
+  const endOfDay = new Date(selectedDate);
+  endOfDay.setHours(23, 59, 59, 999); // this creates a new date object and ends 1ms just before midnight
 
   // we need to find the specific availability
-  // the reason we use firstFirst is bacuz  
+  // the reason we use firstFirst is bacuz
   const data = await prisma.availability.findFirst({
     where: {
       // here we want to find the availability where the day matches
       // to make the date dynamic, we have to get the selectedDate and note that its an object - it needs to be formatted
       // in the Availability table schema - Day is an enum not a string
       // HACK: this Prisma.EnumDayFilter say that `day` the same type as the `enum Day`
-      day: currentDay as Prisma.EnumDayFilter, 
+      day: currentDay as Prisma.EnumDayFilter,
       User: {
         userName: userName,
-      }
+      },
     },
     select: {
       fromTime: true,
@@ -35,56 +38,159 @@ async function getData(userName: string, selectedDate: Date){
         select: {
           grantEmail: true,
           grantId: true,
-        }
-      }
-    }
+        },
+      },
+    },
   });
 
   // with the help of Nylas calendar api
-  const nylasCalendarData =  await nylas.calendars.getFreeBusy({
+  const nylasCalendarData = await nylas.calendars.getFreeBusy({
     identifier: data?.User?.grantId as string, // gives nylas the rights to search our calendar
-    requestBody: { // tell nylas what time range to look at
+    requestBody: {
+      // tell nylas what time range to look at
       // we have to first get the fromTime and endTime
       startTime: Math.floor(startOfDay.getTime() / 1000), // converts js milisecond timestamp to UNIX base timestamp for Nylas
       // divide 1000 converts ms into s to match Unix
       // Math.floor gives whole number
       endTime: Math.floor(endOfDay.getTime() / 1000),
-      emails: [data?.User?.grantEmail as string] // nylas now knows what calendar associated with the email to search up 
+      emails: [data?.User?.grantEmail as string], // nylas now knows what calendar associated with the email to search up
       // grantEmail is the user personal email authenticated with Nylas
-    }
-  })
+    },
+  });
 
   return {
     data,
     nylasCalendarData,
-  }
+  };
 }
 
 interface timeTableProps {
   selectedDate: Date;
   userName: string;
+  duration: number;
+}
+
+function calculateAvailableTimeSlots(
+  date: string,
+  dbAvailability: {
+    // make sure from & till Time spelling are exactly the same
+    fromTime: string | undefined;
+    tillTime: string | undefined;
+  },
+  nylasData: NylasResponse<GetFreeBusyResponse[]>,
+  duration: number,
+) {
+  const now = new Date();
+
+  // convert db availability to a js date object
+  // import parse ONLY from date-fns
+  // NOTE: currently we get date and dbAvailability through params & they are currently just names
+  // they dont contain any values yet - we will pass them as an arg later when we call calculateAvailableTimeSlots function
+  const availableFrom = parse(
+    // we can get the date & dbAvailability through our params
+    `${date} ${dbAvailability.fromTime}`,
+    "yyyy-MM-dd HH:mm",
+    new Date(),
+  );
+
+  const availableTill = parse(
+    `${date} ${dbAvailability.tillTime}`,
+    "yyyy-MM-dd HH:mm",
+    new Date()
+  )
+
+  // this equals to nylas data which we have to get first
+  const busySlots = nylasData.data[0].timeSlots.map((slot) => (
+    {
+      start: fromUnixTime(slot.startTime),
+      end: fromUnixTime(slot.endTime)
+    }
+  ))
+
+  // now we get all possible timeSlots for available time
+  const allSlots = []
+  let currentSlot = availableFrom
+  // isBefore imported from date-fns
+  while (isBefore(currentSlot, availableTill)) {
+    allSlots.push(currentSlot)
+    // addMinutes imported from date-fns
+    currentSlot = addMinutes(currentSlot, duration) // here we add a possible window duration time for a meeting as well
+    // by generating the timeslots based on the duration that we have
+    // to do that, we can get the duration through the params by adding it to this parent function
+  }
+
+
+  const freeSlots = allSlots.filter((slot) => { // filters allSlots array and creates a new array
+    // iterate the array using filter
+    // calculate the end time of each slot by adding duration
+    const slotEnd = addMinutes(slot, duration);
+
+    return (
+      // slot should only start at the current time
+      isAfter(slot, now) &&
+      !busySlots.some(
+        // check slot overlap with Busy Periods which we get from Nylas
+        (busy: { start: any; end: any }) =>
+          (!isBefore(slot, busy.start) && isBefore(slot, busy.end)) || // if slots starts with busy period
+          (isAfter(slotEnd, busy.start) && !isAfter(slotEnd, busy.end)) || // if slots ends with busy period
+          (isBefore(slot, busy.start) && isAfter(slotEnd, busy.end)) // if slots is within the busy period
+        // if none of these are true, then its just a free slot
+      )
+    )
+  });
+
+
+  // Return the formatted Freeslots
+  return freeSlots.map((slot) => format(slot, "HH:mm"));
+
 }
 
 // we can use async here cuz this is a server component
 // nth is leaked to the client, everything is server side
-export async function TimeTable({ selectedDate, userName }: timeTableProps) {
+export async function TimeTable({ selectedDate, userName , duration }: timeTableProps) {
   // getData() takes in a username as a param - which we dont have yet
   // so add userName in our interface, destrcuture it and we are gtg
-  const { data, nylasCalendarData } = await getData(userName, selectedDate)
+  const { data, nylasCalendarData } = await getData(userName, selectedDate);
 
-  console.log(nylasCalendarData)
+  const formattedDate = format(selectedDate, "yyyy-MM-dd")
+  const dbAvailability = {
+    fromTime: data?.fromTime,
+    tillTime: data?.tillTime,
+  }
+
+  const availableSlots = calculateAvailableTimeSlots(
+    formattedDate,
+    dbAvailability,
+    nylasCalendarData,
+    duration,
+  );
 
   return (
     <div>
       <p className="text-base font-semibold">
-        {format(selectedDate, "EEE")} 
-        {" "}
-        <span className="text-md text-muted-foreground ">{format(selectedDate, "MMM. d")}</span>
+        {format(selectedDate, "EEE")}{" "}
+        <span className="text-md text-muted-foreground">
+          {format(selectedDate, "MMM. d")}
+        </span>
       </p>
       {/* to now render the time table - we need to fetch the data */}
 
+      <div className="mt-3 max-h-[350px] overflow-y-auto  ">
+        {availableSlots.length > 0 ? (
+          availableSlots.map((slot, index) => (
+            <Link href={`?date=${format(selectedDate, "yyyy-MM-dd" )}&time=${slot}`} key={index}>
+              <Button className="w-full mb-2"  variant="outline">
+                {slot}
+              </Button>
+            </Link>
+          ))
+        ) : (
+          <p>
+            No Time slots available
+          </p>
+        )}
 
-
+      </div>
     </div>
   );
 }
